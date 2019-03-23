4e6620df3f7f549c75547e5e7eee272f0a624109
Matt Williams <matt.seongjun@gmail.com>
2019-03-23 13:36:44 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,5 +2,8 @@ def display_board(board)
   i = 0
   2.times do
     puts " #{board[i]} | #{board[i+1]} | #{board[i+2]} "
-    puts ""
+    puts "-----------"
     i = i + 3
+  end
+  puts " #{board[i]} | #{board[i+1]} | #{board[i+2]} "
+end
